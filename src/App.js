e91f5e32447d45dcee47b46c78494aba670209ca
Roberto <ritorito141516@gmail.com>
2019-08-22 12:00:34 -0500

    Aprendo Componentes de Clase

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import Table from './Table';
 class App extends Component{
     render(){
         return(
-            <div className="App">
-                <h1>Hola mundo</h1>
-                <div className="Table">
-                    <Table />
-                </div>
+            <div className="Table">
+                <Table />
             </div>
-            
         )
     }
 }
