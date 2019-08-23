c54c85917a0c2ada63d91ad453051bbcb75dadef
Roberto <ritorito141516@gmail.com>
2019-08-23 18:30:06 -0500

    Aprendo Estados

--- src/Table.js
+++ src/Table.js
@@ -17,23 +17,26 @@ import React, {Component} from 'react'
                 <tr key = {index}>
                     <td>{row.name}</td>
                     <td>{row.job}</td>
+                    <td>
+                        <button onClick={() => props.removeCharacter(index)}>Eliminar</button>
+                    </td>
                 </tr>
             )
         })
         return(
             <tbody>{rows}</tbody>
         )
-    }
+    } 
 
 
     class Table extends Component {
         render() {
-            const { characterData } = this.props
+            const { characterData, removeCharacter } = this.props
         
             return (
             <table>
                 <TableHeader />
-                <TableBody characterData={characterData} />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
             </table>
             )
         }
