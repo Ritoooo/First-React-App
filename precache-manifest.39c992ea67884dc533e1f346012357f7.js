63b0dfd27145d2dea363abcc943551e906ca2a9d
Ritoooo <ritorito141516@gmail.com>
2019-08-25 20:15:23 -0500

    Updates

---
+++ precache-manifest.39c992ea67884dc533e1f346012357f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221984add07f4a5fad77763bf8bb0832",
+    "url": "/First-React-App%3E/index.html"
+  },
+  {
+    "revision": "419dc5350919b3c548bb",
+    "url": "/First-React-App%3E/static/css/main.b8765e77.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/First-React-App%3E/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "419dc5350919b3c548bb",
+    "url": "/First-React-App%3E/static/js/main.0c6f454a.chunk.js"
+  },
+  {
+    "revision": "e096acce46dbebab3a57",
+    "url": "/First-React-App%3E/static/js/runtime~main.97e414e0.js"
+  }
+]);
